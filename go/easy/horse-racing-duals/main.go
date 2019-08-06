572583bbda3a53a3de853572fcb1063ec5ed415e
Fabien Bellanger <valentil@gmail.com>
2019-08-06 23:26:54 +0200

    Pas assez rapide

--- go/easy/horse-racing-duals/main.go
+++ go/easy/horse-racing-duals/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -10,13 +12,40 @@ import "fmt"
 func main() {
     var n int
     fmt.Scan(&n)
-    
+	
+	gap := 10000000
+	list := make([]int, 0)
+	stop := false
+
     for i := 0; i < n; i++ {
         var pi int
-        fmt.Scan(&pi)
-    }
+		fmt.Scan(&pi)
+		
+		if !stop {
+			for _, v := range list {
+				var g int
+
+				if pi >= v {
+					g = pi - v
+				} else {
+					g = v - pi
+				}
+
+				if g < gap {
+					gap = g
+					break
+				}
+			}
+
+			if gap == 1 {
+				stop = true
+			}
+
+			list = append(list, pi)
+		}
+	}
     
 	// Output
 	// ------
-    fmt.Println("answer") // Write answer to stdout
+    fmt.Println(gap) // Write answer to stdout
 }
