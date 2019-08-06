a59e9c58700caca100009a787c2cb57c4df63ac0
Fabien Bellanger <valentil@gmail.com>
2019-08-06 22:47:12 +0200

    Fini

---
+++ go/easy/isbn-check-digit/go.mod
@@ -0,0 +1,3 @@
+module github.com/fabienbellanger/codinGameSolutions/go/easy/isbn-check-digit
+
+go 1.12
--- go/easy/isbn-check-digit/main.go
+++ go/easy/isbn-check-digit/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
-import "os"
-import "bufio"
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "regexp"
+    "strconv"
+)
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -16,17 +20,25 @@ func main() {
     var n int
     scanner.Scan()
     fmt.Sscan(scanner.Text(), &n)
+
+    invalidISBN := make([][]byte, 0)
     
     for i := 0; i < n; i++ {
         scanner.Scan()
         // isbn := scanner.Text()
         isbn := scanner.Bytes()
         ok := checkISBN(isbn)
-        println(ok)
+        if !ok {
+            invalidISBN = append(invalidISBN, isbn)
+        }
     }
     
-    // fmt.Fprintln(os.Stderr, "Debug messages...")
-    fmt.Println("answer") // Write answer to stdout
+    // Output
+    // ------
+    fmt.Printf("%d invalid:\n", len(invalidISBN))
+    for _, val := range invalidISBN {
+        fmt.Printf("%s\n", val)
+    }
 }
 
 // checkISBN checks if ISBN is correct
@@ -37,7 +49,69 @@ func checkISBN(s []byte) bool {
     }
 
     // String only have number
-    
+    if match, _ := regexp.Match(`^[\dX]+$`, s); !match {
+        return false
+    }
+
+    if len(s) == 10 {
+        // ISBN 10
+        // -------
+        sum := 0
+        for i := 0; i < 9; i++ {
+            num, err := strconv.Atoi(string(s[i]))
+            if err != nil {
+                return false
+            }
+
+            sum += num * (10 - i)
+        }
+
+        var controlDigit string
+        if sum % 11 == 0 {
+            controlDigit = "0"
+        } else {
+            if 11 - sum % 11 == 10 {
+                controlDigit = "X"
+            } else {
+                controlDigit = strconv.Itoa(11 - sum % 11)
+            }
+        }
+        lastDigit := string(s[9])
+
+        if controlDigit != lastDigit {
+            return false
+        }
+    } else {
+        // ISBN 13
+        // -------
+        sum := 0
+        for i := 0; i < 12; i++ {
+            num, err := strconv.Atoi(string(s[i]))
+            if err != nil {
+                return false
+            }
+
+            if i % 2 == 0 {
+                sum += num
+            } else {
+                sum += num * 3
+            }
+        }
+        
+        controlDigit := 10 - sum % 10
+        if controlDigit == 10 {
+            controlDigit = 0
+        }
+        lastDigit, err := strconv.Atoi(string(s[12]))
+        
+        if err != nil {
+            return false
+        }
+
+        if controlDigit != lastDigit {
+            return false
+        }
+    }
 
     return true
 }
