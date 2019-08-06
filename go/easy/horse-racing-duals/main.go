0b19a6c2f37dfcd9e0683f9788ad205a121be306
Fabien Bellanger <valentil@gmail.com>
2019-08-06 22:59:52 +0200

    Début Chevaux de course

---
+++ go/easy/horse-racing-duals/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+    
+    for i := 0; i < n; i++ {
+        var pi int
+        fmt.Scan(&pi)
+    }
+    
+	// Output
+	// ------
+    fmt.Println("answer") // Write answer to stdout
+}
