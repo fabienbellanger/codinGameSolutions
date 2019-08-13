1e6191224f849799bada450df2a34924b3158e6f
Fabien Bellanger <valentil@gmail.com>
2019-08-13 17:46:47 +0200

    Amélioration

--- go/easy/ascii-art/main.go
+++ go/easy/ascii-art/main.go
@@ -26,34 +26,6 @@ func main() {
 	// Alphabet en ASCII Art
 	// ---------------------
 	var letters [27][]string
-	lettersMatch := map[string]int{
-		"A": 0,
-		"B": 1,
-		"C": 2,
-		"D": 3,
-		"E": 4,
-		"F": 5,
-		"G": 6,
-		"H": 7,
-		"I": 8,
-		"J": 9,
-		"K": 10,
-		"L": 11,
-		"M": 12,
-		"N": 13,
-		"O": 14,
-		"P": 15,
-		"Q": 16,
-		"R": 17,
-		"S": 18,
-		"T": 19,
-		"U": 20,
-		"V": 21,
-		"W": 22,
-		"X": 23,
-		"Y": 24,
-		"Z": 25,
-	}
 	
     for i := 0; i < H; i++ {
         scanner.Scan()
@@ -77,11 +49,9 @@ func main() {
 	// -------------------------------
 	for i := 0; i < H; i++ {
 		for _, letter := range text {
-			letterStr := string(letter)
-
 			index := 26
-			if i, ok := lettersMatch[letterStr]; ok {
-				index = i
+			if letter >= 'A' && letter <= 'Z' {
+				index = int(letter - 'A')
 			}
 			
 			for j := i * L; j < i * L + L; j++ {
