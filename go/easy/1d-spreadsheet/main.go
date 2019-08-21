4b01215c2f6835bfc8b4e21f2b5c5abe36260b40
Fabien Bellanger <valentil@gmail.com>
2019-08-21 23:15:35 +0200

    WIP

--- go/easy/1d-spreadsheet/main.go
+++ go/easy/1d-spreadsheet/main.go
@@ -1,89 +1,36 @@
 package main
 
-import "fmt"
-import "os"
-import "bufio"
-import "strings"
-import "math"
-import "strconv"
+import (
+    "fmt"
+    "os"
+)
 
- type defibrilatorType struct {
-    ID          int
-    Name        string
-    Address     string
-    PhoneNumber string
-    Longitude   float64
-    Latitude    float64
- }
+/*
+Solution test
+-------------
+144
+156
+12
+24
+288
+12
+*/
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Buffer(make([]byte, 1000000), 1000000)
-
-    var longitudeStrFrom string
-    scanner.Scan()
-    fmt.Sscan(scanner.Text(),&longitudeStrFrom)
-    longitudeFrom := strToFloat(longitudeStrFrom)
-    
-    var latitudeStrFrom string
-    scanner.Scan()
-    fmt.Sscan(scanner.Text(),&latitudeStrFrom)
-    latitudeFrom := strToFloat(latitudeStrFrom)
-    
     var N int
-    scanner.Scan()
-    fmt.Sscan(scanner.Text(),&N)
+    fmt.Scan(&N)
+
+    results := make([]int, N)
+    fmt.Fprintf(os.Stderr, "%v\n", results)
     
-    var nearestDefibrilator defibrilatorType
-    distanceMin := 1000000.0
+    for i := 0; i < N; i++ {
+        var operation, arg1, arg2 string
+        fmt.Scan(&operation, &arg1, &arg2)
+    }
 
     for i := 0; i < N; i++ {
-        scanner.Scan()
-        data := scanner.Text()
-
-        array := strings.Split(data, ";")
-        if len(array) == 6 {
-            id, _ := strconv.Atoi(array[0])
-
-            defibrilator := defibrilatorType{
-                ID:          id,
-                Name:        array[1],
-                Address:     array[2],
-                PhoneNumber: array[3],
-                Longitude:   strToFloat(array[4]),
-                Latitude:    strToFloat(array[5]),
-            }
-
-            distance := getDistance(longitudeFrom, latitudeFrom, defibrilator.Longitude, defibrilator.Latitude)
-
-            if distance < distanceMin {
-                distanceMin = distance
-                nearestDefibrilator = defibrilator
-            }
-        }
+        
+        // fmt.Fprintln(os.Stderr, "Debug messages...")
+        fmt.Println("1")// Write answer to stdout
     }
-    
-    fmt.Println(nearestDefibrilator.Name)
-}
-
-func degreeToRadian(d float64) float64 {
-    return d * math.Pi / 180
-}
-
-func strToFloat(s string) float64 {
-    f, _ := strconv.ParseFloat(strings.Replace(s, ",", ".", -1), 64)
-
-	return f
-}
-
-func getDistance(longitudeFrom, latitudeFrom, longitudeTo, latitudeTo float64) (d float64) {
-    longitudeFrom = degreeToRadian(longitudeFrom)
-    latitudeFrom = degreeToRadian(latitudeFrom)
-    longitudeTo = degreeToRadian(longitudeTo)
-    latitudeTo = degreeToRadian(latitudeTo)
-
-    x := (longitudeTo - longitudeFrom) * math.Cos((latitudeFrom + latitudeTo) / 2)
-    y := latitudeTo - latitudeFrom
-
-    return 6371 * math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
 }
