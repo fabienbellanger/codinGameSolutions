902bc411b260bc25d4f9136cea58a4b6591627a2
Fabien Bellanger <valentil@gmail.com>
2020-06-20 00:31:38 +0200

    Pas assez performant

--- rust/easy/van-ecks-sequence/main.rs
+++ rust/easy/van-ecks-sequence/main.rs
@@ -5,6 +5,12 @@ macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
+#[derive(Debug)]
+struct Diff {
+    before: i32,
+    after: i32,
+}
+
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
@@ -14,38 +20,38 @@ fn main() {
     let n = parse_input!(input_line, i32);
 
     let mut sequence = vec![a1];
-    for i in a1+1..n {
-        let last = last_item(&sequence);
-        let mut val: i32 = if let Some(index) = &sequence[..i as usize-2].iter().rposition(|&r| r == *last) {
-            let j = *index as i32;
-            if j > i - 1 {
-                j
-            } else {
-                -1
-            }
-        } else {
-            -1
+    let mut values: HashMap<i32, Diff> = HashMap::new();
+    values.insert(a1, Diff{before: 0, after: 0});
+    for i in 1..n {
+        let last = match sequence.last() {
+            Some(l) => *l,
+            None => 0i32,
         };
+        
+        let next = match values.get(&last) {
+            Some(next) => next,
+            None => &Diff{before: 0, after: 0},
+        };
+        
+        let new_val = next.after - next.before;
+        sequence.push(new_val);
 
-        if val == -1 {
-            eprintln!("last={} - i={} : index={:?} => {}", last, i, val, 0);
-            sequence.push(0);
-        } else {
-            eprintln!("last={} - i={} : index={:?} => {}", last, i, val, i - val);
-            sequence.push(i - val);
-        }
+        // let new_diff = if let Some(v) = values.get(&new_val) {
+        //     Diff{before: v.after, after: i}
+        // } else {
+        //     Diff{before: i, after: i}
+        // };
+        // values.insert(new_val, new_diff);
+        //values.entry(new_val).or_insert(Diff{before: i, after: i});
+        //values.insert(new_val, Diff{before: values[&new_val].after, after: i});
+        *values.entry(new_val).or_insert(Diff{before: i, after: i}) = Diff{before: values[&new_val].after, after: i};
+
     }
 
-    let last = last_item(&sequence);
-    
-    eprintln!("sequence={:?}\n", sequence);
-    
-    println!("{}", last);
-}
-
-fn last_item(list: &Vec<i32>) -> &i32 {
-    match list.last() {
+    let last = match sequence.last() {
         Some(l) => l,
         None => &0i32,
-    }
+    };
+    
+    println!("{}", last);
 }
