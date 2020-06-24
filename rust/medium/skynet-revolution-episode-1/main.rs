c80b1c9a6227278efb0b80fff6809797204937e9
Fabien Bellanger <valentil@gmail.com>
2020-06-24 12:25:55 +0200

    Début Skynet Revolution - Episode 1

---
+++ rust/medium/skynet-revolution-episode-1/main.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let n = parse_input!(inputs[0], i32); // the total number of nodes in the level, including the gateways
+    let l = parse_input!(inputs[1], i32); // the number of links
+    let e = parse_input!(inputs[2], i32); // the number of exit gateways
+    eprintln!("n={}, l={}, e={}", n, l, e);
+
+    for i in 0..l as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let n1 = parse_input!(inputs[0], i32); // N1 and N2 defines a link between these nodes
+        let n2 = parse_input!(inputs[1], i32);
+        eprintln!("n1={}, n2={}", n1, n2);
+    }
+    for i in 0..e as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let ei = parse_input!(input_line, i32); // the index of a gateway node
+        eprintln!("ei={}", ei);
+    }
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let si = parse_input!(input_line, i32); // The index of the node on which the Skynet agent is positioned this turn
+        eprintln!("si={}", si);
+        
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+
+        // Example: 0 1 are the indices of the nodes you wish to sever the link between
+        println!("0 1");
+    }
+}
