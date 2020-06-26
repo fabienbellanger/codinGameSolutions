3f63c46ee8bb71c6274fba8d6120d1641a578fc7
Fabien Bellanger <valentil@gmail.com>
2020-06-26 18:49:43 +0200

    Mise à jour du code

--- rust/medium/stock-exchange-losses/main.rs
+++ rust/medium/stock-exchange-losses/main.rs
@@ -7,12 +7,30 @@ macro_rules! parse_input {
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let n = parse_input!(input_line, i32);
+    let n = parse_input!(input_line, usize);
     let mut inputs = String::new();
     io::stdin().read_line(&mut inputs).unwrap();
+
+    let mut values: Vec<i32> = Vec::new();
+    let mut diff: Vec<i32> = Vec::new();
+    let mut j: usize = 0;
     for i in inputs.split_whitespace() {
         let v = parse_input!(i, i32);
+        values.push(v);
+
+        if values.len() > 1 {
+            diff.push(v - values[j - 1]);
+        }
+
+        j += 1;
+    }
+    eprintln!("values={:?}, diff={:?}", values, diff);
+
+    for i in 1..n-1 {
+        eprintln!("diff={:?}", diff[i]);
     }
 
-    println!("answer");
+    let loss = 0i32;
+    
+    println!("{}", loss);
 }
